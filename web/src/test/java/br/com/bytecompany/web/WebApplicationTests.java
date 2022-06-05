c67c3001fcd7260034241d9d002dea370225826a
aljsjunca <aljs.junca@gmail.com>
2022-06-05 18:20:54 -0300

    Inicialização do Spring Boot

---
+++ web/src/test/java/br/com/bytecompany/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bytecompany.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
