ae170df214355540db9e5899154f3691fd5ab237
aljsjunca <aljs.junca@gmail.com>
2022-06-06 20:18:11 -0300

    Criação da primeira página com injeção em Thymeleaf, SpringBoot no java

--- web/src/main/java/br/com/bytecompany/web/controller/HomeController.java
+++ web/src/main/java/br/com/bytecompany/web/controller/HomeController.java
@@ -1,13 +1,15 @@
 package br.com.bytecompany.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String homeApp() {
+	public String homeApp(Model model) {
+		model.addAttribute("mensagem", "Está mensagme foi injetada pelo Modelo");
 		return "index";
 	}
 	
