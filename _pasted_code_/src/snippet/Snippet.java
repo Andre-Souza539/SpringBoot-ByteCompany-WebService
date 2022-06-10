25cff82a6e0b36d353980217f1c0c2d2e64d63c4
aljsjunca <aljs.junca@gmail.com>
2022-06-10 15:32:45 -0300

    criação do método de Delete para o CRUD

---
+++ _pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,6 @@
+package snippet;
+
+public class Snippet {
+	<form th:object="${commandObject}" th:action="@{/urlToCall}" th:method="delete">
+}
+
--- web/src/main/java/br/com/bytecompany/web/ClienteController.java
+++ web/src/main/java/br/com/bytecompany/web/ClienteController.java
@@ -1,14 +1,20 @@
 package br.com.bytecompany.web;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
+import org.hibernate.annotations.Parameter;
+import org.hibernate.sql.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ClienteController {
@@ -36,6 +42,19 @@ public class ClienteController {
 		}
 		clienteRepository.save(cliente);
 		return "redirect:/clientes";
+	}
+	
+	@GetMapping("/cliente/delete/{id}")
+	public String deleteCliente(@PathVariable Long id) {
+		clienteRepository.deleteById(id);
+		return "redirect:/clientes";
+	}
+	
+	@GetMapping("/cliente/edit/{id}")
+	public String editCliente(@PathVariable Long id, Model model) {
+			
+		
+		return "UpdateForm";
 	}
 	
 }
